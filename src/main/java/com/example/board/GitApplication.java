48fb535a09017958a33976fbc12d1e81194b2184
kimjiwoong76 <46660207+kimjiwoong76@users.noreply.github.com>
2021-04-06 22:48:45 +0900

    내려받기 테스트

--- src/main/java/com/example/board/GitApplication.java
+++ src/main/java/com/example/board/GitApplication.java
@@ -10,6 +10,7 @@ public class GitApplication {
 		SpringApplication.run(GitApplication.class, args);
 		System.out.println("최초의 PUSH");
 		System.out.println("두번째 PUSH");
+		System.out.println("내려받기 테스트");
 	}
 
 }
